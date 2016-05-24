0d226fca486a89cea02a54a7c26eec826c7bc0a4
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2016-05-24 12:23:58 +0300

    Added ability slugify cirrilic filenames

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Slugifier – utilities set for slugify
 =================
 
-Django helper for slugify model's ImageField filenames
+Django helper for slugify model's FileField and ImageField filenames
 
-Use tag 0.0.1 for Django 1.6 and tag 0.0.2 for Django 1.7
+### Compability:
+
+* Use tag 0.0.1 for Django 1.6
--- slugifier/utils.py
+++ slugifier/utils.py
@@ -1,6 +1,9 @@
 import os
+
+from unidecode import unidecode
 from django.utils.text import slugify
 from django.utils.deconstruct import deconstructible
+
 
 @deconstructible
 class UploadToSlugify(object):
@@ -10,8 +13,16 @@ class UploadToSlugify(object):
 
     def __call__(self, instance, filename):
         name, ext = os.path.splitext(filename)
-        f = slugify(name) + ext
+        f = slugify(unidecode(name)) + ext
         return os.path.join(self.path, f)
+
+    def normalizy(self, name):
+        """
+        normalizy name to filename compatibility
+        """
+        name = unidecode(name)
+        return slugify(name.decode('utf-8'))
+
 
 # Backward compatibility
 def upload_to_slugify(path="/"):
