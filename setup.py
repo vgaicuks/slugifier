ee84dff04ece37dd5d241cb98315ba5656792eef
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2018-05-09 12:29:51 +0300

    Bump up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='slugifier',
-      version='3.0.0',
+      version='3.0.1',
       description='Slugify utils for django projects.',
       long_description='Slugify utils for django projects.',
       author=u'Vitālijs Gaičuks',
